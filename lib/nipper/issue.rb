eb8f30470396610e26dd7c735583343a709b4d14
Aaron Manaloto <aaronpomanaloto@gmail.com>
2020-09-14 14:27:12 +0800

    Add CVSS score and fix typo

--- lib/nipper/issue.rb
+++ lib/nipper/issue.rb
@@ -8,6 +8,7 @@ module Nipper
       [
         :title,
         :cvss_base, :cvss_temporal, :cvss_environmental,
+        :cvss_base_vector, :cvss_temporal_vector, :cvss_environmental_vector,
         :finding, :impact, :ease, :recommendation
       ]
     end
@@ -23,25 +24,40 @@ module Nipper
         return
       end
 
-      if method == :title
-        return @xml.attr('title')
-      else
-        return process_field_value(method)
-      end
+      return process_field_value(method)
     end
 
     def process_field_value(method)
       translations_table = {
-        cvss_base: 'issuedetails/ratings/cvssv2-base',
-        cvss_temporal: 'issuedetails/ratings/cvssv2-temporal',
-        cvss_environmental: 'issuedetails/ratings/cvssv2-environmental',
         finding: 'section[@ref="FINDING"]/text',
         impact: 'section[@ref="IMPACT"]/text',
         ease: 'section[@ref="EASE"]/text',
         recommendation: 'section[@ref="RECOMMENDATION"]/text'
       }
 
-      @xml.xpath("./#{translations_table[method].to_s}").first.try(:text)
+      if method == :title
+        @xml.attr('title')
+      elsif method.to_s.starts_with?('cvss')
+        process_cvss_field(method)
+      else
+        @xml.xpath("./#{translations_table[method].to_s}").first.try(:text)
+      end
+    end
+
+    def process_cvss_field(method)
+      translations_table = {
+        cvss_base: 'issuedetails/ratings/cvssv2-base',
+        cvss_temporal: 'issuedetails/ratings/cvssv2-temporal',
+        cvss_environmental: 'issuedetails/ratings/cvssv2-environmental',
+      }
+
+      base_method = method.to_s.sub('_vector', '').to_sym
+
+      if method.to_s.ends_with?('vector')
+        @xml.xpath("./#{translations_table[base_method].to_s}").first.try(:text)
+      else
+        @xml.xpath("./#{translations_table[base_method].to_s}").attr('score')
+      end
     end
   end
 end
