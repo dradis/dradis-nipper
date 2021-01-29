c69f421ba3e0718181d5346f5fd1168887eb7811
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-01-29 23:18:31 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/nipper/engine.rb
+++ lib/dradis/plugins/nipper/engine.rb
@@ -1,13 +1,9 @@
-module Dradis
-  module Plugins
-    module Nipper
-      class Engine < ::Rails::Engine
-        isolate_namespace Dradis::Plugins::Nipper
+module Dradis::Plugins::Nipper
+  class Engine < ::Rails::Engine
+    isolate_namespace Dradis::Plugins::Nipper
 
-        include ::Dradis::Plugins::Base
-        description 'Processes Nipper XML format'
-        provides :upload
-      end
-    end
+    include ::Dradis::Plugins::Base
+    description 'Processes Nipper XML format'
+    provides :upload
   end
 end
